ff04fa9efe0b48575e3bf355743d5d4d65c24db9
adriansgn <drianngunsa@gmail.com>
2022-12-19 12:58:59 +0800

    added requirements.txt

--- pychat/__main__.py
+++ pychat/__main__.py
@@ -12,7 +12,7 @@ def main():
 
     print("Pychat v0.1")
     query = input('Input: ')
-    completion = openai.Completion.create(engine='text-davinci-003', prompt=query, max_tokens= 4000)
+    completion = openai.Completion.create(engine='text-davinci-003', prompt=query, max_tokens=4000)
     output = completion.choices[0].text
     print("Output: {}".format(output))
 
