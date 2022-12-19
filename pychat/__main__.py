a9944ffe45d2b3b17fbcd51dca8d932c34e94327
adriansgn <drianngunsa@gmail.com>
2022-12-19 18:18:32 +0800

    added while loop for enhancement. parameterized max_tokens,engine, and query. Resolves #1 Resolves #2

--- pychat/__main__.py
+++ pychat/__main__.py
@@ -1,20 +1,50 @@
 import openai
 import os
+import argparse
 
-OPENAI_API_KEY='OPENAI_API_KEY'
+OPENAI_API_KEY = 'OPENAI_API_KEY'
 
 def main():
     openai_api_key = os.getenv(OPENAI_API_KEY)
+
+    parser = argparse.ArgumentParser(description="Pychat v0.1")
+    parser.add_argument("--max-tokens", help="Maximum size of tokens to be used", type=int, default=4000)
+    parser.add_argument("--engine", help="The openai engine to use", type=str, default='text-davinci-003')
+    parser.add_argument("--query", help="checkinng for user input", type=str,required=True)
+
+
+    args = parser.parse_args()
+    max_tokens = args.max_tokens #flag galing sa parser.add_argument
+    engine = args.engine
+    query = args.query
 
     if openai_api_key == None:
         print('OPENAI_API_KEY required')
         exit(-1)
 
-    print("Pychat v0.1")
-    query = input('Input: ')
-    completion = openai.Completion.create(engine='text-davinci-003', prompt=query, max_tokens=4000)
-    output = completion.choices[0].text
-    print("Output: {}".format(output))
+    while query != 'quit':
+        test_input = query.strip()
+
+        if test_input:
+            completion = openai.Completion.create(
+                engine=engine, prompt=query, max_tokens=max_tokens
+            )
+            if len(completion.choices) == 0:
+                print("No output")
+
+            else:
+                output = completion.choices[0].text
+                print("Output: {}".format(output))
+
+            query = input("Input (quit to exit): ").strip()
+
+        else:
+            print('Invalid input')
+
+    print("\nPowered by openai")
+
+
+
 
 if __name__ == '__main__':
     main()
